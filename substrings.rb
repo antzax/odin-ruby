ab86b72de27ace190c1a1dce8f6b6746caa062ae
antzax <166389859+antzax@users.noreply.github.com>
2025-03-09 11:24:58 +0200

    clean up

--- substrings.rb
+++ substrings.rb
@@ -4,17 +4,25 @@ def substrings(substring, dictionary)
   dictionary.each do |word|
       if substring.downcase.include?(word)
         word_count = substring.scan(/#{word}/i).count
-        hash = Hash[word => word_count]
-        result.merge!(hash)
+        result[:"#{word}"] = word_count
       end
   end
-  
+
   return result
 end
 
+
+# TEST DATA
 substring = "Howdy partner, sit down! How's it going?"
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-
 test = { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
+test_count = test.values.count
+
+# RESULT
 answer = substrings(substring, dictionary)
-puts test == answer
+answer_count = answer.values.count
+
+# TEST
+if answer_count == test_count
+  puts "OK"
+end
